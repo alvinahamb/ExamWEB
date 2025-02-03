a900d4bccba7bbeb0021c32939274c66f3685e3b
BlessedRamaroson777 <blessedramaroson@gmail.com>
2025-02-03 15:37:51 +0300

    Ajout _elevage

--- sql/script.sql
+++ sql/script.sql
@@ -3,15 +3,16 @@ CREATE DATABASE IF NOT EXISTS Elevage;
 USE Elevage;
 
 -- Table des utilisateurs
-CREATE TABLE Utilisateur (
+CREATE TABLE Utilisateur_Elevage (
     IdUtilisateur INT AUTO_INCREMENT PRIMARY KEY,
     Email VARCHAR(255) NOT NULL UNIQUE,
     Nom VARCHAR(100) NOT NULL,
     MotDePasse VARCHAR(255) NOT NULL,
-    Numero VARCHAR(15) NOT NULL
+    Numero VARCHAR(15) NOT NULL,
+    Capital int not null 
 );
 
-CREATE TABLE Admin (
+CREATE TABLE Admin_Elevage (
     IdAdmin INT AUTO_INCREMENT PRIMARY KEY,
     Email VARCHAR(255) NOT NULL UNIQUE,
     Nom VARCHAR(100) NOT NULL,
@@ -19,7 +20,7 @@ CREATE TABLE Admin (
 );
 
 -- Table des animaux
-CREATE TABLE Animaux (
+CREATE TABLE Animaux_Elevage (
     IdAnimal INT AUTO_INCREMENT PRIMARY KEY,
     TypeAnimal VARCHAR(50) NOT NULL,
     PoidsMin FLOAT NOT NULL,
@@ -30,7 +31,7 @@ CREATE TABLE Animaux (
 );
 
 -- Table des aliments
-CREATE TABLE Alimentation (
+CREATE TABLE Alimentation_Elevage (
     IdAliment INT AUTO_INCREMENT PRIMARY KEY,
     NomAliment VARCHAR(100) NOT NULL,
     TypeAnimal VARCHAR(50) NOT NULL,
@@ -40,7 +41,7 @@ CREATE TABLE Alimentation (
 );
 
 -- Table des transactions
-CREATE TABLE TransactionsAnimaux (
+CREATE TABLE TransactionsAnimaux_Elevage (
     IdTransaction INT AUTO_INCREMENT PRIMARY KEY,
     TyeTransaction ENUM('achat', 'vente') NOT NULL,
     DateTransaction DATE NOT NULL,
@@ -49,18 +50,18 @@ CREATE TABLE TransactionsAnimaux (
     IdUtilisateur INT NOT NULL,
     Poids DECIMAL(6,2) NOT NULL,
     Montant_total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (IdAnimal) REFERENCES Animaux(IdAnimal) ON DELETE SET NULL,
-    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs(IdAnimal) ON DELETE SET NULL
+    FOREIGN KEY (IdAnimal) REFERENCES Animaux_Elevage(IdAnimal) ON DELETE SET NULL,
+    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs_Elevage(IdAnimal) ON DELETE SET NULL
 );
 
-CREATE TABLE TransactionsAlimentation (
+CREATE TABLE TransactionsAlimentation_Elevage (
     IdTransaction INT AUTO_INCREMENT PRIMARY KEY,
     DateTransaction DATE NOT NULL,
     IdAliment INT NOT NULL,
     Quantite INT NOT NULL,
     IdUtilisateur INT NOT NULL,
-    FOREIGN KEY (IdAliment) REFERENCES Alimentation(IdAliment) ON DELETE CASCADE, 
-    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs(IdAnimal) ON DELETE SET NULL
+    FOREIGN KEY (IdAliment) REFERENCES Alimentation_Elevage(IdAliment) ON DELETE CASCADE, 
+    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs_Elevage(IdAnimal) ON DELETE SET NULL
 );
 
 INSERT INTO Admin (Email, Nom, MotDePasse) 
