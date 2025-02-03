2ebcc617c248550f28c8d73d183131f1129848cb
BlessedRamaroson777 <blessedramaroson@gmail.com>
2025-02-03 15:31:23 +0300

    Update script

--- sql/script.sql
+++ sql/script.sql
@@ -35,20 +35,32 @@ CREATE TABLE Alimentation (
     NomAliment VARCHAR(100) NOT NULL,
     TypeAnimal VARCHAR(50) NOT NULL,
     PourcentageGainPoids FLOAT NOT NULL,
+    PrixUnitaire DECIMAL(10,2) NOT NULL,
     Stock INT DEFAULT 0
 );
 
 -- Table des transactions
-CREATE TABLE Transactions (
+CREATE TABLE TransactionsAnimaux (
     IdTransaction INT AUTO_INCREMENT PRIMARY KEY,
     TyeTransaction ENUM('achat', 'vente') NOT NULL,
     DateTransaction DATE NOT NULL,
-    IdAnimal INT NULL,
-    IdAliment INT NULL,
-    Quantite INT NOT NULL,
-    Montant FLOAT NOT NULL,
+    Etat INT NULL,
+    IdAnimal INT NOT NULL,
+    IdUtilisateur INT NOT NULL,
+    Poids DECIMAL(6,2) NOT NULL,
+    Montant_total DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (IdAnimal) REFERENCES Animaux(IdAnimal) ON DELETE SET NULL,
-    FOREIGN KEY (IdAliment) REFERENCES Alimentation(IdAliment) ON DELETE SET NULL
+    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs(IdAnimal) ON DELETE SET NULL
+);
+
+CREATE TABLE TransactionsAlimentation (
+    IdTransaction INT AUTO_INCREMENT PRIMARY KEY,
+    DateTransaction DATE NOT NULL,
+    IdAliment INT NOT NULL,
+    Quantite INT NOT NULL,
+    IdUtilisateur INT NOT NULL,
+    FOREIGN KEY (IdAliment) REFERENCES Alimentation(IdAliment) ON DELETE CASCADE, 
+    FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateurs(IdAnimal) ON DELETE SET NULL
 );
 
 INSERT INTO Admin (Email, Nom, MotDePasse) 
