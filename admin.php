d3a4ad49eed04b50bb01fd343d6cd619197a9a35
NyKia398 <153897575+Akinya398@users.noreply.github.com>
2025-02-04 11:44:32 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/connexion.css">
+    <link rel="icon" href="">
+    <title></title>
+</head>
+
+<body>
+    <div class="menu-fixe" style="box-shadow: 5px 5px 5px rgba(126, 126, 126, 0.575);">
+        <div class="logo">
+            <a href="home"><img width="80" height="80" src="/assets/images/logo.png" alt="logo"></a>
+        </div>
+        <div>
+            <ul class="nav nav-tabs nav-justified">
+                <li><a href="modifier">Modifier</a></li>
+                <li><a href="ajouter">Ajouter</a></li>
+                <li><a href="aliments">Aliments</a></li>
+                <li>
+                <form action="deconnexion" method="get">
+                    <button class="button">Deconnexion</button>
+                </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    <div style="margin-top: 15vh;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Admin</h1>
+                    <p>Welcome to the admin page!</p>
+                </div>
+            </div>
+        </div>
+
+        <?php if (isset($extra['message'])): ?>
+            <div id="alert" class="alert alert-success" role="alert"><?= $extra['message'] ?></div>
+        <?php endif; ?>
+        <h2>Liste des animaux</h2>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Espece</th>
+                    <th>Poids Minimal Vente</th>
+                    <th>Poids Maximal</th>
+                    <th>Prix de vente (par kg)</th>
+                    <th>Nombre de jour sans manger</th>
+                    <th>% perte de poids</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($data as $animaux) : ?>
+                    <tr>
+                        <td><?= $animaux['TypeAnimal'] ?></td>
+                        <td><?= $animaux['PoidsMin'] ?></td>
+                        <td><?= $animaux['PoidsMax'] ?></td>
+                        <td><?= $animaux['PrixVenteParKg'] ?>$</td>
+                        <td><?= $animaux['JoursSansManger'] ?></td>
+                        <td><?= $animaux['PourcentagePertePoids'] ?></td>
+                        <td>
+                            <?php if (!empty($animaux['Image'])) : ?>
+                                <img src="<?php echo file_exists('/assets/images/' . $animaux['Image']) ? '/assets/images/' . $animaux['Image'] : '/uploads/' . $animaux['Image']; ?>" alt="Image" width="100">
+                            <?php else : ?>
+                                <span>Pas d'image</span>
+                            <?php endif; ?>
+                        </td>
+                        <td class="table-actions">
+                            <a href="delete?id=<?= $animaux['IdAnimal'] ?>">Supprimer</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <footer>
+        <p>Kasaina ETU003287 & Blessed ETU003326 & Kiady ETU003244</p>
+    </footer>
+</body>
+
+</html>
---
+++ adminLogin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="/assets/css/admin.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
+</head>
+<body>
+    <!-- <div class="menu-fixe">
+        <div class="logo">
+            <a href="home"><img width="80" height="80" src="/assets/images/logo.png" alt="logo"></a>
+        </div>
+    </div> -->
+    <div style="margin-top: 15vh;">
+        <?php if (isset($erreur)) { ?>
+            <div id="alert" class="alert alert-danger" role="alert"><?= $erreur ?></div>
+        <?php }
+        ?><br><br>
+        <div class="card">
+            <br><br><h2>Admin</h2>
+            <h2><div class="card-info">
+                <form action="adminLogin" method="POST">
+                <div class="input-container">
+        <input id="username" name="username" class="input-field" type="text" value="admin" required>
+        <label for="username" class="input-label">Username</label>
+        <span class="input-highlight"></span>
+    </div>
+    <div class="input-container">
+        <input id="password" name="password" class="input-field" type="password" value="blessed" required>
+        <label for="password" class="input-label">Password</label>
+        <span class="input-highlight"></span>
+    </div>
+                    <p><button class="button" type="submit">Login<div class="hoverEffect"><div></div></div></button></p>
+                    
+                </form>
+            </div><h2>
+        </div>
+    </div>
+</body>
+<footer>
+        <p>&copy; <?= date('Y') ?>Kasaina ETU003287 & Blessed ETU003326 & Kiady ETU003244</p>
+    </footer>
+</html>
