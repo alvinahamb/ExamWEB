83586d613d974d7eb07eac59dac49bcd350aff32
BlessedRamaroson777 <blessedramaroson@gmail.com>
2025-02-04 09:41:43 +0300

    verification getanimauxbyuserdate

--- app/models/ElevageModel.php
+++ app/models/ElevageModel.php
@@ -212,59 +212,6 @@ class ElevageModel
         }
     }
     
-    // public function getAnimauxByUserDate($id, $date)
-    // {
-    //     $stmt = $this->db->prepare("
-    //         SELECT a.IdAnimal, a.TypeAnimal, a.PoidsMin, a.PoidsMax, a.Poids, a.PrixVenteParKg, 
-    //             a.JoursSansManger, a.PourcentagePertePoids, t.DateTransaction
-    //         FROM TransactionsAnimaux_Elevage t
-    //         JOIN Animaux_Elevage a ON t.IdAnimal = a.IdAnimal
-    //         WHERE t.IdUtilisateur = ?
-    //         AND t.TypeTransaction = 'achat'
-    //         AND t.DateTransaction = (
-    //             SELECT MAX(t2.DateTransaction)
-    //             FROM TransactionsAnimaux_Elevage t2
-    //             WHERE t2.IdAnimal = t.IdAnimal
-    //                 AND t2.DateTransaction <= ?
-    //         )
-    //     ");
-    //     $stmt->execute([$id, $date]);
-    //     $animals = $stmt->fetchAll();
-    
-    //     foreach ($animals as &$animal) {
-    //         if (empty($animal['DateTransaction'])) {
-    //             $animal['Vivant'] = "Non"; 
-    //             continue;
-    //         }
-    
-    //         $dateTransaction = new \DateTime($animal['DateTransaction']);
-    //         $dateNow = new \DateTime($date);
-    //         $diff = $dateTransaction->diff($dateNow)->days;
-    
-    //         if ($diff > $animal['JoursSansManger']) {
-    //             $animal['Vivant'] = "Non"; // L'animal est mort
-    //         } else {
-    //             $animal['Vivant'] = "Oui"; // L'animal est vivant
-    //         }
-    
-    //         if ($diff > 0) {
-    //             // Perte de poids par jour en fonction du pourcentage
-    //             $poidsPerdu = $diff * ($animal['PourcentagePertePoids'] / 100);
-    //             $nouveauPoids = $animal['Poids'] - $poidsPerdu;
-    
-    //             // Si le poids tombe en dessous du poids minimal, on rétablit le poids minimal
-    //             if ($nouveauPoids < $animal['PoidsMin']) {
-    //                 $nouveauPoids = $animal['PoidsMin'];
-    //             }
-    
-    //             // Mise à jour du poids de l'animal après la perte
-    //             $animal['Poids'] = $nouveauPoids;
-    //         }
-    //     }
-    
-    //     return $animals;
-    // }
-
     public function venteAnimaux($id, $idAnimal, $idUser, $date)
     {
         $animals = $this->getAnimauxByUserDate($idUser, $date);
